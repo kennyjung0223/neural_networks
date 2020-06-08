484803f41f196b9abd1dfbcaa01954b841508946
kennyjung0223 <kenrohs@gmail.com>
2020-06-08 10:35:41 -0700

    added main and fixed bugs

--- xor_nn.py
+++ xor_nn.py
@@ -26,8 +26,8 @@ def forward_prop(X, parameters):
 
 	Z1 = np.dot(W1, X) + b1
 	A1 = np.tanh(Z1)
-	Z2 = np.dot(W2, X) + b2
-	A2 = np.sigmoid(Z2)
+	Z2 = np.dot(W2, A1) + b2
+	A2 = sigmoid(Z2)
 
 	cache = {
 		"A1": A1,
@@ -58,7 +58,7 @@ def backward_prop(X, Y, cache, parameters):
 	grads = {
 		"dW1": dW1,
 		"db1": db1,
-		"dW2": dw2,
+		"dW2": dW2,
 		"db2": db2,
 	}
 
@@ -70,9 +70,9 @@ def update_parameters(parameters, grads, learning_rate):
 	W2 = parameters["W2"]
 	b2 = parameters["b2"]
 
-	dW1 = grads["W1"]
+	dW1 = grads["dW1"]
 	db1 = grads["db1"]
-	dW2 = grads["W2"]
+	dW2 = grads["dW2"]
 	db2 = grads["db2"]
 
 	W1 = W1 - learning_rate*dW1
@@ -102,7 +102,7 @@ def model(X, Y, n_x, n_h, n_y, num_of_iters, learning_rate):
 		parameters = update_parameters(parameters, grads, learning_rate)
 
 		if i % 100 == 0:
-			print("Cost after iteration# {:d}: {:f}".format(i, cost))
+			print("Cost after iteration {:d}: {:f}".format(i, cost))
 
 	return parameters
 
@@ -119,5 +119,32 @@ def predict(X, parameters):
 	return y_predict
 
 
+np.random.seed(2)
+
+# The 4 training examples by columns
+X = np.array([[0,0,1,1], [0,1,0,1]])
+
+# The outputs of the XOR for every example in X
+Y = np.array([0,1,1,0])
+
+# No. of training examples
+m = X.shape[1]
+# Set the hyperparameters
+n_x = 2		# No. of neurons in first layer
+n_h = 2		# No. of neurons in hidden layer
+n_y = 1		# No. of neurons in output layer
+
+num_of_iters = 1000
+learning_rate = 0.3
+
+trained_parameters = model(X, Y, n_x, n_h, n_y, num_of_iters, learning_rate)
+
+# Test 2X1 vector to calculate the XOR of its elements
+# Can try: (0, 0), (0, 1), (1, 0), (1, 1)
+X_test = np.array([[1], [1]])
+y_predict = predict(X_test, trained_parameters)
+# Print the result
+print("Neural Network prediction for example ({:d}, {:d}) is {:d}".format(
+	X_test[0][0], X_test[1][0], y_predict))
 
 
